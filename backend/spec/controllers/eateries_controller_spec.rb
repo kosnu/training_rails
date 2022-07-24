31a78c93dca1d9617aeac91a91de4db52cc7f7d9
Kosuke Numata <coursek8814@gmail.com>
2022-07-25 01:26:34 +0900

    :sparkles: 飲食店を追加できるようにした

--- backend/spec/controllers/eateries_controller_spec.rb
+++ backend/spec/controllers/eateries_controller_spec.rb
@@ -4,10 +4,10 @@ require 'rails_helper'
 require 'support/shared/sign_in'
 
 RSpec.describe EateriesController, type: :request do
+  include_context 'sign_in'
+
   describe 'GET /eateries' do
     subject(:request) { get eateries_path }
-
-    include_context 'sign_in'
 
     context '正常時' do
       before do
@@ -22,6 +22,57 @@ RSpec.describe EateriesController, type: :request do
       it '飲食店の一覧がレスポンスに含まれていること' do
         request
         expect(response.body).to include 'Test Eatery'
+      end
+    end
+  end
+
+  describe 'GET /eatery/new' do
+    subject(:request) { get new_eatery_path }
+
+    context '正常時' do
+      it 'アクセスできること' do
+        request
+        expect(response).to have_http_status :ok
+      end
+    end
+  end
+
+  describe 'POST /eateries' do
+    subject(:request) { post eateries_path(request_params) }
+
+    let(:request_params) do
+      {
+        eatery: {}
+      }
+    end
+
+    context '正常時' do
+      let(:request_params) do
+        {
+          eatery: {
+            name: eatery_name,
+            address: eatery_address
+          }
+        }
+      end
+
+      let(:eatery_name) { 'ABC' }
+      let(:eatery_address) { 'Osaka' }
+
+      it 'アクセスできること' do
+        request
+        expect(response).to have_http_status :ok
+      end
+
+      it '飲食店を追加できること' do
+        request
+        expect(Eatery.find_by(name: eatery_name, address: eatery_address)).to be_present
+      end
+
+      it 'ログインユーザーと紐づいていること' do
+        request
+        eatery = Eatery.find_by(name: eatery_name, address: eatery_address)
+        expect(eatery.users).to include sign_in_user
       end
     end
   end
